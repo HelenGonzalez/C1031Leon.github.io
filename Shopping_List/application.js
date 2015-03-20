fe05bc685244b48e61214d4b68cd2d202f80988b
C1031Leon <CLeon@kippnyccp.org>
2015-03-20 13:31:56 -0400

    Update application.js

--- Shopping_List/application.js
+++ Shopping_List/application.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
     var entry = $('input').val();
     $('ul').append("<li>"+entry+"</li>");
   })
+ $('#Empty').on('click', function(){
+    var entry = $('input').val();
+    $('ul').remove("li");
+  })
+  
 });
